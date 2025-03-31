281458e874220e87dbf4122ac440ed60d5b88e1c
SaSue <sandro@suechting.com>
2025-04-01 01:24:14 +0200

    Update clone_raspi_git.sh

--- clone_raspi_git.sh
+++ clone_raspi_git.sh
@@ -1,5 +1,4 @@
 rm -rf /opt/raspi-status
 cd /opt
 git clone https://github.com/SaSue/raspi-status.git
-sudo chmod +x raspi-status/client/client-status-update.sh
-
+sudo chmod +x raspi-status/client/*.sh
